035858b2e2a2609fa82d1f69d6e0f76b2428457a
avinash-peakmind <avinash.dwivedi@peakmind.in>
2025-03-12 17:18:49 +0530

    added cors domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", 'http://3.7.45.143/', 'http://3.7.45.143:8000/'],
+    domains: ["localhost", 'http://3.7.45.143/', 'http://3.7.45.143:8000/', 'http://3.7.45.143', '3.7.45.143'],
     remotePatterns: [
       {
         protocol: "https",
