a7a6ec1135f7b6fae003e23bc5319c2330257745
deepakyadav121999 <dy44444221@gmail.com>
2025-06-25 14:58:27 +0530

    build changes

--- src/auth.ts
+++ src/auth.ts
@@ -98,7 +98,7 @@
 //   },
 // });
 
-import NextAuth from "next-auth";
+// import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
